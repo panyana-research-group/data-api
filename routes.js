321a0cd8079e1293b6c174a9616d2e0a990af85d
Glitch (hello-express) <none>
2019-01-31 04:37:28 +0000

    🚗😤 Checkpoint ./routes.js:1794594/397

--- routes.js
+++ routes.js
@@ -1,5 +1,14 @@
+const sheets = {
+  lore: {
+    id: "1ThxEY2LC8Rg9WUd53aWXrpe8wsprnEwVyFLKb6QFf0k"
+  }
+}
+
 module.exports = app => {
   app.get("/api/sheets/:name", (req, res) => {
+    if (!sheets[req.params.name]) {
+      res.status(500).json({ error: "Not a valid sheet name" })
+    }
     console.log(req.params.name)
   })
   
