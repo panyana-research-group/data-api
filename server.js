97b7dae5e3f793d8cac8bde6c908ec0bae971fea
Glitch (hello-express) <none>
2019-01-31 04:05:28 +0000

    🛍🍪 Checkpoint ./package.json:1794594/1389 ./server.js:1794594/719 ./routes.js:1794594/8

--- server.js
+++ server.js
@@ -1,19 +1,33 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const { google } = require('googleapis')
+const routes = require('./routes.js')
 const app = express();
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
 
-app.post('/api', (req, res) => {
-  console.log(req.body)
-  res.send({
-    message: "Success"
-  })
+routes(app)
+
+const jwtClient = new google.auth.JWT(
+  process.env.CLIENT_EMAIL,
+  null,
+  process.env.PRIVATE_KEY.replace(/\\n/g, "\n"),
+  [
+    "https://www.googleapis.com/auth/drive",
+    "https://www.googleapis.com/auth/spreadsheets"
+  ]
+)
+
+jwtClient.authorize((err, token) => {
+  if (err)
+    return console.error(err)
+  else
+    console.log("Google JWT successfully connected!")
 })
 
 const listener = app.listen(process.env.PORT, function() {
   console.log('Your app is listening on port ' + listener.address().port);
-});
+})
