1937e5fce8fb05f879e9c81bc6f351b1a4ae475a
Glitch (hello-express) <none>
2019-02-06 07:46:54 +0000

    🍎🍂 Checkpoint ./routes/index.js:1794594/939 ./server.js:1794594/33

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,27 @@
 const lore = require('./lore')
+const { google } = require('googleapis')
+const sheets = google.sheets('v4')
 
-module.exports = (app, db) => {
+module.exports = (app, db, jwt) => {
   lore(app, db)
+  
+  app.get('/convert', (req, res) => {
+    sheets.spreadsheets.values.get({
+      auth: jwt,
+      spreadsheetId: "1ThxEY2LC8Rg9WUd53aWXrpe8wsprnEwVyFLKb6QFf0k",
+      range: "Lore!A2:H"
+    }, (err, data) => {
+      if (err) {
+        console.error(err)
+        res.status(500).send(err)
+      }
+      else {
+        for (let i = 0; i < 3; i++) {
+          
+        }
+        console.log("Success: Get sheet: " + req.params.name)
+        res.status(200).send(data.data.values)
+      }
+    })
+  })
 }
