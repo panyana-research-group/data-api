a7fd122c585fb67be6445d775a1abb5f4a48fe32
Glitch (hello-express) <none>
2019-02-21 22:02:27 +0000

    👘🚄 Checkpoint ./routes/index.js:1794594/2686

--- routes/index.js
+++ routes/index.js
@@ -7,65 +7,87 @@ const ObjectID = require('mongodb').ObjectID
 module.exports = (app, db, jwt, upload) => {
   lore(app, db, jwt, upload)
   
-  app.get('/convert', (req, res) => {
-    db.collection('lore').updateOne(
-      { _id: new ObjectID(
-    // db.collection('lore').find().toArray((err, allItems) => {
-    //   if (err) res.status(500).send(err)
-    //   else {
-    //     const promises = []
-    //     res.status(200).send('OK')
-    //     allItems.forEach(item => {
-    //       item.missingWiki = item.missingWiki.split(',').join(', ')
-    //       item.missingPics = item.missingPics.split(',').join(', ')
-    //       item.addWiki = item.addWiki.split(',').join(', ')
-    //       console.log(item)
-    //       // promises.push(
-    //       //   db.collection('lore').updateOne(
-    //       //     { _id: new ObjectID(item._id) },
-    //       //     { $set:
-    //       //       {
-    //       //         missingWiki: item.missingWiki.split(',').join(', '),
-    //       //       }
-    //       //     }
-    //       //   )
-    //       // )
-    //     })
-    //   }
-    // })
-  })
+//   app.get('/convert', (req, res) => {
+//     // db.collection('lore').findOne(
+//     //   { _id: new ObjectID('5c5a92e060ed193ca1ae33d6') },
+//     //   (err, item) => {
+//     //     if (err) res.status(500).send(err)
+//     //     else {
+//     //       db.collection('lore').updateOne(
+//     //         { _id: new ObjectID(item._id) },
+//     //         { $set: 
+//     //          {
+//     //            missingWiki: item.missingWiki.split(',').join(', '),
+//     //            missingPics: item.missingPics.split(',').join(', '),
+//     //            addWiki: item.addWiki.split(',').join(', ')
+//     //          }
+//     //         }
+//     //       ).then(r => {
+//     //         res.status(200).send(r)
+//     //       }).catch(e => {
+//     //         res.status(500).send(e)
+//     //       })
+//     //     }
+//     //   }
+//     // )
+//   //   db.collection('lore').find().toArray((err, allItems) => {
+//   //     if (err) res.status(500).send(err)
+//   //     else {
+//   //       const promises = []
+//   //       allItems.forEach(item => {
+//   //         promises.push(
+//   //           db.collection('lore').updateOne(
+//   //             { _id: new ObjectID(item._id) },
+//   //             { $set:
+//   //               {
+//   //                 missingWiki: item.missingWiki.split(',').join(', '),
+//   //                 missingPics: item.missingPics.split(',').join(', '),
+//   //                 addWiki: item.addWiki.split(',').join(', ')
+//   //               }
+//   //             }
+//   //           )
+//   //         )
+//   //       })
+//   //       Promise.all(promises).then(r => {
+//   //         res.status(200).send(r)
+//   //       }).catch(e => {
+//   //         res.status(500).send(e)
+//   //       })
+//   //     }
+//   //   })
+//   // })
   
-  // app.get('/convert', (req, res) => {
-  //   sheets.spreadsheets.values.get({
-  //     auth: jwt,
-  //     spreadsheetId: "1ThxEY2LC8Rg9WUd53aWXrpe8wsprnEwVyFLKb6QFf0k",
-  //     range: "Lore!A2:H"
-  //   }, (err, data) => {
-  //     if (err) {
-  //       console.error(err)
-  //       res.status(500).send(err)
-  //     }
-  //     else {
-  //       const sheet = data.data.values
-  //       const stories = []
-  //       for (let i = 0; i < sheet.length; i++) {
-  //         stories.push({
-  //           title: sheet[i][0],
-  //           onWiki: sheet[i][1],
-  //           missingWiki: sheet[i][2],
-  //           missingPics: sheet[i][3],
-  //           addWiki: sheet[i][4],
-  //           folderId: sheet[i][7]
-  //         })
-  //       }
-  //       db.collection('lore').insertMany(stories).then(result => {
-  //         console.log(result)
-  //         res.status(200).send(stories)
-  //       }).catch(err => {
-  //         console.log(err)
-  //         res.status(500).send(err)
-  //       })
-  //     }
-  //   })
-  // })
+//   // app.get('/convert', (req, res) => {
+//   //   sheets.spreadsheets.values.get({
+//   //     auth: jwt,
+//   //     spreadsheetId: "1ThxEY2LC8Rg9WUd53aWXrpe8wsprnEwVyFLKb6QFf0k",
+//   //     range: "Lore!A2:H"
+//   //   }, (err, data) => {
+//   //     if (err) {
+//   //       console.error(err)
+//   //       res.status(500).send(err)
+//   //     }
+//   //     else {
+//   //       const sheet = data.data.values
+//   //       const stories = []
+//   //       for (let i = 0; i < sheet.length; i++) {
+//   //         stories.push({
+//   //           title: sheet[i][0],
+//   //           onWiki: sheet[i][1],
+//   //           missingWiki: sheet[i][2],
+//   //           missingPics: sheet[i][3],
+//   //           addWiki: sheet[i][4],
+//   //           folderId: sheet[i][7]
+//   //         })
+//   //       }
+//   //       db.collection('lore').insertMany(stories).then(result => {
+//   //         console.log(result)
+//   //         res.status(200).send(stories)
+//   //       }).catch(err => {
+//   //         console.log(err)
+//   //         res.status(500).send(err)
+//   //       })
+//   //     }
+//   //   })
+//   // })
 }
