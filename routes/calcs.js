bf16626d20abc67fa73c5519bc0b981366ce1d46
Jake Potrebic <jake.m.potrebic@gmail.com>
2019-02-23 23:37:13 -0700

    finished 2nd calc

--- routes/calcs.js
+++ routes/calcs.js
@@ -5,56 +5,81 @@ const mats = require('../data/materials')
 
 module.exports = app => {
   app.post('/calcs/engine/mats', (req, res) => {
+    const optimal = optimalMats(req.body.engine)
+    if (optimal) res.status(200).send({ res: 'success', data: optimal })
+    else res.status(200).send({ res: 'none found', data: null })
+  })
+
+  app.post('/calcs/engine/cipher', (req, res) => {
     const engine = req.body.engine
     let maxSpeed = 0
     let optimal = null
 
-    for (let m = 0; m < mats.length; m++) {
-      for (let co = 0; co < mats.length; co++) {
-        for (let ca = 0; ca < mats.length; ca++) {
-          for (let p = 0; p < mats.length; p++) {
-            const pwr = engine.pwr +
-              engine.pwr * mats[m].boosts.engine.mech.pwr +
-              engine.pwr * mats[co].boosts.engine.comb.pwr
-            const oh = engine.oh + 
-              engine.oh * mats[m].boosts.engine.mech.oh +
-              engine.oh * mats[co].boosts.engine.comb.oh
-            const cf = coolingFactor(mats[ca].cf, mats[p].cf)
+    const pwrOH = 177 - engine.res - engine.fe - engine.su // get possible other points
+    let pwrPoints = pwrOH - 100 < 5 ? 5 : pwrOH - 100
 
-            const w = weight('engine', engine, {
-              casing: mats[ca].weight,
-              mech: mats[m].weight,
-              comb: mats[co].weight,
-              prop: mats[p].weight
-            })
+    for (pwrPoints; pwrPoints <= (pwrOH < 100 ? pwrOH - 5 : 100); pwrPoints++) {
+      let ohPoints = pwrOH - pwrPoints
+      const result = optimalMats({ res: engine.res, pwr: pwrPoints, oh: ohPoints, su: engine.su, fe: engine.fe })
+      if (result && result.speed > maxSpeed) {
+        maxSpeed = result.speed
+        optimal = { ...result, points: { pwr: pwrPoints, oh: ohPoints }}
+      }
+    }
 
-            let speed = 50 * Math.sqrt((2 * pwr) / w)
+    if (optimal) res.status(200).send({ res: 'success', data: optimal})
+    else res.status(200).send({ res: 'none found', data: null })
+  })
+}
 
-            if (!isNaN(ohTime(pwr, oh, cf))) {
-              speed = 0
-            }
-            if (speed > maxSpeed) {
-              maxSpeed = speed
-              optimal = {
-                casing: mats[ca].name,
-                mech: mats[m].name,
-                comb: mats[co].name,
-                prop: mats[p].name,
-                speed: round(speed, 2),
-                weight: round(w, 2)
+function optimalMats(points) {
+  let maxSpeed = 0
+  let optimal = null
+  for (let m = 0; m < mats.length; m++) {
+    for (let co = 0; co < mats.length; co++) {
+      for (let ca = 0; ca < mats.length; ca++) {
+        for (let p = 0; p < mats.length; p++) {
+          const pwr = points.pwr +
+            points.pwr * mats[m].boosts.engine.mech.pwr +
+            points.pwr * mats[co].boosts.engine.comb.pwr
+          const oh = points.oh + 
+            points.oh * mats[m].boosts.engine.mech.oh +
+            points.oh * mats[co].boosts.engine.comb.oh
+          const cf = coolingFactor(mats[ca].cf, mats[p].cf)
+
+          const w = weight('engine', points, {
+            casing: mats[ca].weight,
+            mech: mats[m].weight,
+            comb: mats[co].weight,
+            prop: mats[p].weight
+          })
+
+          let speed = 50 * Math.sqrt((2 * pwr) / w)
+
+          if (!isNaN(ohTime(pwr, oh, cf))) {
+            speed = 0
+          }
+          if (speed > maxSpeed) {
+            maxSpeed = speed
+            optimal = {
+              casing: mats[ca].name,
+              mech: mats[m].name,
+              comb: mats[co].name,
+              prop: mats[p].name,
+              speed: round(speed, 2),
+              weight: round(w, 2),
+              crafted: {
+                pwr,
+                oh,
+                cf
               }
             }
           }
         }
       }
     }
-    if (optimal) res.status(200).send({ res: 'success', data: optimal })
-    else res.status(200).send({ res: 'none found', data: null })
-  })
-
-  app.get('/calcs/engine/cipher', (req, res) => {
-    console.log(req.body)
-  })
+  }
+  return optimal
 }
 
 function round(value, decs) {
