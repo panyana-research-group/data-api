a520565d3f32af50c948c58895bd327226102f35
Jake Potrebic <jake.m.potrebic@gmail.com>
2019-04-30 23:47:15 -0700

    role stuff

--- routes/auth.js
+++ routes/auth.js
@@ -15,7 +15,7 @@ module.exports = app => {
           res.status(200).send(response)
         })
         .catch(err => {
-          console.log(err)
+          console.error(err)
           res.status(500).send(err)
         })
     } else {
@@ -37,6 +37,43 @@ module.exports = app => {
         res.status(200).send(response)
       })
       .catch(err => {
+        console.error(err)
+        res.status(500).send(err)
+      })
+  })
+
+  app.get('/auth/roles/:id', (req, res) => {
+    if (req.params.id === 'all') {
+      management
+        .getRoles()
+        .then(response => {
+          res.status(200).send(response)
+        })
+        .catch(err => {
+          console.error(err)
+          res.status(500).send(err)
+        })
+    } else {
+      management
+        .getRole({ id: req.params.id })
+        .then(response => {
+          res.status(200).send(response)
+        })
+        .catch(err => {
+          console.error(err)
+          res.status(500).send(err)
+        })
+    }
+  })
+
+  app.get('/auth/roles/:id/users', (req, res) => {
+    management
+      .getUsersInRole({ id: req.params.id })
+      .then(response => {
+        res.status(200).send(response)
+      })
+      .catch(err => {
+        console.error(err)
         res.status(500).send(err)
       })
   })
